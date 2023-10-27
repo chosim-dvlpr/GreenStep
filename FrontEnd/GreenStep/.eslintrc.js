3c4ca199d7202300ea62ae4d3477bd91fc26a678
Minji Byun <jwww98730@naver.com>
2023-10-27 17:25:03 +0900

    style: style 컴포넌트 추가

--- FrontEnd/GreenStep/.eslintrc.js
+++ FrontEnd/GreenStep/.eslintrc.js
@@ -1,7 +1,18 @@
-module.exports = {
+// module.exports = {
+//   root: true,
+//   extends: '@react-native',
+//   rules: {
+//     'prettier/prettier': 0,
+//   },
+// };
+
+
+// ES6 문법
+export default {
   root: true,
   extends: '@react-native',
   rules: {
+    'quotes': ['error', 'single'], // ESLint가 단일 따옴표를 요구하며, 이러한 규칙을 어긴 경우 해당 오류를 표시하지 않게 됩니다.
     'prettier/prettier': 0,
   },
 };
--- FrontEnd/GreenStep/src/Style/Box.tsx
+++ FrontEnd/GreenStep/src/Style/Box.tsx
@@ -3,37 +3,37 @@ import { StyleSheet } from 'react-native'
 const Box = StyleSheet.create({
   /** 플로깅 진행 중 화면 위쪽의 정보 박스 */
   ploggingInfoBox: {
-    width: "100%",
+    width: '100%',
     padding: 20,
-    backgroundColor: "lightgreen",
+    backgroundColor: 'lightgreen',
     borderRadius: 20,
   },
   /** 플로깅 데이터 (세개짜리) 하나하나의 박스 */
   ploggingDataInfoBox: {
-    width: "30%",
-    backgroundColor: "yellow",
+    width: '30%',
+    backgroundColor: 'yellow',
     padding: 20,
     borderRadius: 20,
   },
   /** 업적 리스트, 게시글 리스트 박스 */
   cardBox: {
-    width: "100%",
+    width: '100%',
     padding: 20,
-    backgroundColor: "skyblue",
+    backgroundColor: 'skyblue',
     borderRadius: 10,
   },
   /** 내가 작성한 게시글 박스 */
   mediumCardBox: {
-    width: "30%",
-    backgroundColor: "yellow",
+    width: '30%',
+    backgroundColor: 'yellow',
     padding: 20,
     borderRadius: 15,
   },
   /** 경쟁 기여도 부분 초록색 박스 */
   competitionBox: {
-    width: "100%",
+    width: '100%',
     padding: 30,
-    backgroundColor: "gray",
+    backgroundColor: 'gray',
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
   }
