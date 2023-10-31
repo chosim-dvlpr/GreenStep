0c148ebad1b0d3e86b80cdfd55932b33931a0608
이주언 <wndjs8225@naver.com>
2023-10-31 10:36:00 +0900

    Front fe component button

---
+++ FrontEnd/GreenStep/src/Style/ButtonStyle.tsx
@@ -0,0 +1,48 @@
+import { StyleSheet } from "react-native"
+
+const ButtonStyle = StyleSheet.create({
+  /** 작은 크기의 버튼
+   * 크루찾기 메인 - 글 쓰기
+   * 플로깅 종료 후 업적 모달 - 확인 버튼
+   * 업적카테고리 버튼
+   */
+  smallButton: {
+    width: 100,
+    height: 45,          
+    borderRadius: 15,
+    alignItems : 'center',
+    justifyContent: 'center'
+  },
+    /** 중간 크기의 버튼
+     * 플로깅 중 모달 - 등록버튼
+     * 메인페이지 -플로깅 하기 
+     */
+    middleButton: {
+        width: '60%',
+        height: 55,
+        paddingHorizontal: 10,
+        paddingVertical: 0,
+        borderRadius: 15,
+        alignItems : 'center',
+        justifyContent: 'center'
+      },
+  /** 큰 크기의 버튼
+   * 가로 길이 꽉차는 버튼
+   * 플로깅 종료 - 인증하기 버튼
+   * 크루찾기 - 참여하기, 글 쓰기
+   */
+  largeButton: {
+    width: '90%', 
+    height: 60,       
+    borderRadius: 15,
+    alignItems : 'center',
+    justifyContent: 'center'
+  },
+//   flex: {
+//     flexDirection : 'row',
+//     justifyContent: 'space-around'
+//     // textAlign:'auto',
+//   },
+})
+
+export default ButtonStyle;
