9fae3aeeb5c70913772d3ddc4aa09169a2476105
Minji Byun <jwww98730@naver.com>
2023-11-01 17:43:14 +0900

    feat: 플로깅 종료화면 사진 업로드 버튼 추가

--- FrontEnd/GreenStep/src/Page/PloggingFinish.tsx
+++ FrontEnd/GreenStep/src/Page/PloggingFinish.tsx
@@ -1,6 +1,7 @@
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
 import React from 'react';
 import styled from 'styled-components/native';
+import ButtonStyle from '../Style/ButtonStyle';
 
 const HeaderText = styled.Text`
   font-size: 30;
@@ -13,20 +14,42 @@ const HeaderTextContainer = styled.View`
   margin-left: 30;
 `
 
+const UploadPhotoButtonContainer = styled.View`
+  margin-left: 20;
+  margin-right: 20;
+`
+
 const PloggingFinish = () => {
+  const uploadPhoto = () => {
+    console.log('사진 업로드 버튼 클릭')
+  };
+
   return (
     <View>
       {/* 헤더 */}
       <HeaderTextContainer>
         <HeaderText>오늘도 해냈어요!</HeaderText>
       </HeaderTextContainer>
-
+ 
       {/* 플로깅 데이터 */}
 
       {/* 인증하기 버튼 */}
+      <UploadPhotoButtonContainer>
+        <TouchableOpacity
+        onPress={() => uploadPhoto()}
+        style={[
+          ButtonStyle.whiteColor, 
+          ButtonStyle.fullLargeButton
+        ]}
+        >
+          <Text 
+          style={{color: '#8BCA84', fontWeight: 'bold', fontSize: 20}}
+          >인증하기</Text>
+        </TouchableOpacity>
+      </UploadPhotoButtonContainer>
 
       {/* 인증 사진/회색 빈 칸 */}
-      
+
     </View>
   );
 };
