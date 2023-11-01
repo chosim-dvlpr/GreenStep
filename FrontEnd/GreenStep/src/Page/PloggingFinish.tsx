9058ced2e4af4e454f2c3c0302fba490f4599744
Minji Byun <jwww98730@naver.com>
2023-11-01 17:25:24 +0900

    feat: 플로깅 종료화면 헤더 및 기능 목록 작성

--- FrontEnd/GreenStep/src/Page/PloggingFinish.tsx
+++ FrontEnd/GreenStep/src/Page/PloggingFinish.tsx
@@ -1,10 +1,32 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import styled from 'styled-components/native';
+
+const HeaderText = styled.Text`
+  font-size: 30;
+  font-weight: bold;
+  color: black;
+`
+
+const HeaderTextContainer = styled.View`
+  margin-top: 50;
+  margin-left: 30;
+`
 
 const PloggingFinish = () => {
   return (
     <View>
-      <Text>PloggingFinish</Text>
+      {/* 헤더 */}
+      <HeaderTextContainer>
+        <HeaderText>오늘도 해냈어요!</HeaderText>
+      </HeaderTextContainer>
+
+      {/* 플로깅 데이터 */}
+
+      {/* 인증하기 버튼 */}
+
+      {/* 인증 사진/회색 빈 칸 */}
+      
     </View>
   );
 };
