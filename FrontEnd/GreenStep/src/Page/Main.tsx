c0f16cf37613e49f859d9c85f4b8814f0520f410
Minji Byun <jwww98730@naver.com>
2023-11-01 14:16:19 +0900

    design: 메인 메세지 추가 및 css 변경

--- FrontEnd/GreenStep/src/Page/Main.tsx
+++ FrontEnd/GreenStep/src/Page/Main.tsx
@@ -15,13 +15,29 @@ import {
 } from 'react-native';
 import Login from '../Component/Main/Login';
 import Carousel from '../Component/Main/Carousel';
+import styled from 'styled-components/native';
 
+const MainText = styled.Text`
+  font-size: 30;
+  font-weight: bold;
+  color: black;
+`
+
+const MainTextContainer = styled.View`
+  margin-top: 75;
+  margin-left: 30;
+  margin-bottom: 25;
+`
 
 const Main = () => {
 
   return (
     <View>
-      <Text>Main</Text>
+      <MainTextContainer>
+        <MainText>자연을 지키는</MainText>
+        <MainText>당신과 우리의 발자국</MainText>
+        <MainText>그린스텝</MainText>
+      </MainTextContainer>
       <Carousel/>
       <Login/>
     </View>
