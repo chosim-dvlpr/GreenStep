60150bf99af76c84fd82dcf71c18623ce6912699
Minji Byun <jwww98730@naver.com>
2023-11-02 16:03:11 +0900

    feat: 로그인 api (basicHttp) 추가

---
+++ FrontEnd/GreenStep/src/Api/basicHttp.tsx
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+//기본 axios 인스턴스 생성
+const axiosInstance = axios.create({
+  baseURL: 'https://k9b303.p.ssafy.io/api',
+});
+
+/** 로그인 관련 API */
+export const LoginAPI = {
+  /** 카카오에서 받은 토큰을 백으로 넘겨줌 */
+  getLogin: function (token: string) {
+    return axiosInstance.request({
+      method: "GET",
+      url: '/login',
+    });
+  }
+}
