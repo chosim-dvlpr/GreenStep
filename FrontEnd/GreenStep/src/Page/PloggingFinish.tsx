585b095f6fdb6fc3cce899d6cfb90784cc7bdaac
Minji Byun <jwww98730@naver.com>
2023-11-02 10:24:34 +0900

    feat: 플로깅 데이터 박스 추가 및 css 지정

--- FrontEnd/GreenStep/src/Page/PloggingFinish.tsx
+++ FrontEnd/GreenStep/src/Page/PloggingFinish.tsx
@@ -1,7 +1,12 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, ScrollView, Image} from 'react-native';
 import React from 'react';
 import styled from 'styled-components/native';
 import ButtonStyle from '../Style/ButtonStyle';
+import ProfilePloggingDataInfo from '../Component/Profile/ProfilePloggingDataInfo';
+import ImageStyle from '../Style/Image';
+
+const PloggingFinishContainer = styled.View`
+`
 
 const HeaderText = styled.Text`
   font-size: 30;
@@ -10,47 +15,64 @@ const HeaderText = styled.Text`
 `
 
 const HeaderTextContainer = styled.View`
-  margin-top: 50;
+  margin-top: 60;
   margin-left: 30;
 `
 
-const UploadPhotoButtonContainer = styled.View`
-  margin-left: 20;
-  margin-right: 20;
+const PloggingDataContainer = styled.View`
+  margin-top: 30;
 `
 
+const UploadPhotoButtonContainer = styled.View`
+  width: 86%;
+  margin: auto;
+  margin-top: 30;
+`
+
+
+
 const PloggingFinish = () => {
+
   const uploadPhoto = () => {
     console.log('사진 업로드 버튼 클릭')
   };
 
   return (
-    <View>
-      {/* 헤더 */}
-      <HeaderTextContainer>
-        <HeaderText>오늘도 해냈어요!</HeaderText>
-      </HeaderTextContainer>
- 
-      {/* 플로깅 데이터 */}
+    <ScrollView>
+      <PloggingFinishContainer>
+        {/* 헤더 */}
+        <HeaderTextContainer>
+          <HeaderText>오늘도 해냈어요!</HeaderText>
+        </HeaderTextContainer>
+  
+        {/* 플로깅 데이터 */}
+        <PloggingDataContainer>
+          <ProfilePloggingDataInfo/>
+        </PloggingDataContainer>
 
-      {/* 인증하기 버튼 */}
-      <UploadPhotoButtonContainer>
-        <TouchableOpacity
-        onPress={() => uploadPhoto()}
-        style={[
-          ButtonStyle.whiteColor, 
-          ButtonStyle.fullLargeButton
-        ]}
-        >
-          <Text 
-          style={{color: '#8BCA84', fontWeight: 'bold', fontSize: 20}}
-          >인증하기</Text>
-        </TouchableOpacity>
-      </UploadPhotoButtonContainer>
+        {/* 인증하기 버튼 */}
+        <UploadPhotoButtonContainer>
+          <TouchableOpacity
+          onPress={() => uploadPhoto()}
+          style={[
+            ButtonStyle.whiteColor, 
+            ButtonStyle.fullLargeButton
+          ]}
+          >
+            <Text 
+            style={{color: '#8BCA84', fontWeight: 'bold', fontSize: 20}}
+            >인증하기</Text>
+          </TouchableOpacity>
+        </UploadPhotoButtonContainer>
 
-      {/* 인증 사진/회색 빈 칸 */}
+        {/* 인증 사진/회색 빈 칸 */}
+        <Image 
+        source={{uri: 'https://mediahub.seoul.go.kr/uploads/mediahub/2022/03/nqIdsTmuNLznSsyBFyENToHLigbKWoLx.png'}} 
+        style={ImageStyle.largeImage} />
+        {/* 지도 */}
 
-    </View>
+      </PloggingFinishContainer>
+    </ScrollView>
   );
 };
 
