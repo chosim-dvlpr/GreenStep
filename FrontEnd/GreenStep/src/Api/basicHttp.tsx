8fa18c98cce67c1be8bb51808529063e454b34de
Minji Byun <jwww98730@naver.com>
2023-11-03 17:09:57 +0900

    feat: 로그아웃 axios 추가

--- FrontEnd/GreenStep/src/Api/basicHttp.tsx
+++ FrontEnd/GreenStep/src/Api/basicHttp.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { EmailLoginDataType } from "../Page/Main";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 //기본 axios 인스턴스 생성
 const axiosInstance = axios.create({
@@ -42,4 +43,26 @@ export const MainAPI = {
       url: '/main/picture',
     });
   },
+}
+
+export const TokenAPI = {
+  /** 로그아웃 요청 */
+  logoutAxios: async function() {
+    const tokens = await AsyncStorage.getItem('Tokens')
+    if (tokens) {
+      const parsedTokens = JSON.parse(tokens);
+      const accessToken = parsedTokens.accessToken;
+      const refreshToken = parsedTokens.refreshToken;
+      
+      return axiosInstance.request({
+        method: "POST",
+        url: '/user/logout',
+        data: {
+          'accessToken': accessToken,
+          'refreshToken': refreshToken,
+        }
+      });
+    };
+  },
+  
 }
