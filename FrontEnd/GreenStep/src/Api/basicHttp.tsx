acf99792d19634a1f8817f6778fc2fc1ed7264ca
Minji Byun <jwww98730@naver.com>
2023-11-03 12:35:04 +0900

    feat: 메인 화면 data, 이미지 api 추가

--- FrontEnd/GreenStep/src/Api/basicHttp.tsx
+++ FrontEnd/GreenStep/src/Api/basicHttp.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { EmailLoginDataType } from "../Page/Main";
 
 //기본 axios 인스턴스 생성
 const axiosInstance = axios.create({
@@ -13,5 +14,29 @@ export const LoginAPI = {
       method: "GET",
       url: '/login',
     });
-  }
+  },
+  getEmailLoginAxios: function (data: EmailLoginDataType) {
+    return axiosInstance.request({
+      method: "POST",
+      url: '/login',
+    });
+  },
+}
+
+/** 메인 화면 API */
+export const MainAPI = {
+  /** 메인화면의 총 거리, 시간 조회 */
+  mainDataAxios: function () {
+    return axiosInstance.request({
+      method: "GET",
+      url: '/',
+    });
+  },
+  /** 메인화면의 플로깅 이미지 조회 */
+  mainImageAxios: function () {
+    return axiosInstance.request({
+      method: "GET",
+      url: '/picture',
+    });
+  },
 }
