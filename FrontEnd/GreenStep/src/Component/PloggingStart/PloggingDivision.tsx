18fe4b4adc39cfe3019f0e236f80da33c57f55b7
gangins <kki129443@gmail.com>
2023-11-05 21:49:57 +0900

    feat : start css수정

--- FrontEnd/GreenStep/src/Component/PloggingStart/PloggingDivision.tsx
+++ FrontEnd/GreenStep/src/Component/PloggingStart/PloggingDivision.tsx
@@ -1,12 +1,64 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, TouchableOpacity, Image, Modal} from 'react-native';
+import React, {useState} from 'react';
+import styled from 'styled-components/native';
+import ImageStyle from '../../Style/Image';
+import PloggingModal from './PloggingModal';
 
-const PloggingDivision = () => {
+interface PloggingDivisionProps {
+  name: string;
+  onPress?: () => void; // Optional, 모든 division에 필요한 것은 아님
+}
+function getImage(name: string): any {
+  switch (name) {
+    case 'AI 쓰레기 인식':
+      return require('../../Image/PloggingStart/ai_camera.png');
+    case '병':
+      return require('../../Image/PloggingStart/bottle.png');
+    case '캔':
+      return require('../../Image/PloggingStart/can.png');
+    case '페트병':
+      return require('../../Image/PloggingStart/pet.png');
+    case '플라스틱':
+      return require('../../Image/PloggingStart/plastic.png');
+    case '일반쓰레기':
+      return require('../../Image/PloggingStart/trash.png');
+    case '재활용품':
+      return require('../../Image/PloggingStart/trash_img.png');
+  }
+}
+const PloggingDivision: React.FC<PloggingDivisionProps> = ({name, onPress}) => {
+  const imageSource = getImage(name);
+  const [counts, setCounts] = useState({
+    병: 0,
+    캔: 0,
+    페트병: 0,
+    플라스틱: 0,
+    일반쓰레기: 0,
+  });
+  const handlePress = name => {
+    if (name === '재활용품' && onPress) {
+      onPress();
+    } else {
+      setCounts(prevCounts => ({
+        ...prevCounts,
+        [name]: prevCounts[name] + 1,
+      }));
+      console.log(counts);
+    }
+  };
+
   return (
-    <View>
-      <Text>PloggingDivision</Text>
-    </View>
+    <>
+      <TouchableOpacity onPress={() => handlePress(name)}>
+        <ImageContainer>
+          <Image source={imageSource} style={ImageStyle.smallImage} />
+        </ImageContainer>
+      </TouchableOpacity>
+    </>
   );
 };
 
 export default PloggingDivision;
+const ImageContainer = styled.View`
+  alignitems: center;
+`;
--- FrontEnd/GreenStep/src/Component/PloggingStart/PloggingModal.tsx
+++ FrontEnd/GreenStep/src/Component/PloggingStart/PloggingModal.tsx
@@ -1,12 +1,109 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {
+  Modal,
+  Text,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  ImageBackground,
+} from 'react-native';
+import styled from 'styled-components/native';
+import PloggingDivision from './PloggingDivision';
 
-const PloggingModal = () => {
+interface PloggingModalProps {
+  onClose: () => void;
+  visible: boolean;
+}
+
+const PloggingModal: React.FC<PloggingModalProps> = ({onClose, visible}) => {
   return (
-    <View>
-      <Text>PloggingModal</Text>
-    </View>
+    <Modal
+      visible={visible}
+      transparent={true}
+      animationType="fade"
+      onRequestClose={onClose}>
+      <TouchableWithoutFeedback onPress={onClose}>
+        <ModalOverlay>
+          <TouchableWithoutFeedback>
+            <ModalContainer>
+              <PloggingDivisionContainer>
+                <StyledImageBackground
+                  source={require('../../Image/PloggingStart/bgcircle.png')}
+                  resizeMode="contain" // 또는 "contain", "stretch", "repeat", "center" 중 선택
+                >
+                  <PloggingDivision name="페트병" />
+                </StyledImageBackground>
+                <StyledImageBackground
+                  source={require('../../Image/PloggingStart/bgcircle.png')}
+                  resizeMode="contain" // 또는 "contain", "stretch", "repeat", "center" 중 선택
+                >
+                  <PloggingDivision name="플라스틱" />
+                </StyledImageBackground>
+              </PloggingDivisionContainer>
+              <PloggingDivisionContainer>
+                <StyledImageBackground
+                  source={require('../../Image/PloggingStart/bgcircle.png')}
+                  resizeMode="contain" // 또는 "contain", "stretch", "repeat", "center" 중 선택
+                >
+                  <PloggingDivision name="병" />
+                </StyledImageBackground>
+                <StyledImageBackground
+                  source={require('../../Image/PloggingStart/bgcircle.png')}
+                  resizeMode="contain" // 또는 "contain", "stretch", "repeat", "center" 중 선택
+                >
+                  <PloggingDivision name="캔" />
+                </StyledImageBackground>
+              </PloggingDivisionContainer>
+              <CloseButton onPress={onClose}>
+                <ButtonText>닫기</ButtonText>
+              </CloseButton>
+            </ModalContainer>
+          </TouchableWithoutFeedback>
+        </ModalOverlay>
+      </TouchableWithoutFeedback>
+    </Modal>
   );
 };
 
 export default PloggingModal;
+
+const ModalOverlay = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ModalContainer = styled.View`
+  background-color: white;
+  width: 85%;
+  height: 500px;
+  border-radius: 10px;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PloggingDivisionContainer = styled.View`
+  flex-direction: row;
+
+  margin: 10px;
+`;
+const StyledImageBackground = styled(ImageBackground)`
+  width: 130px;
+  height: 130px;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  margin: 10px;
+`;
+const CloseButton = styled.TouchableOpacity`
+  margin-top: 10px;
+  background-color: red;
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const ButtonText = styled.Text`
+  color: white;
+  font-weight: bold;
+`;
