6c3ff7a1cf0328de2d8a2f84fcbbd8d175933bc9
LeeSW-k <swl9455@naver.com>
2023-11-06 10:58:40 +0900

    feat : Security설정에 OAuth추가

--- greenstep/src/main/java/com/mm/greenstep/domain/common/config/SecurityConfig.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/common/config/SecurityConfig.java
@@ -1,43 +1,70 @@
 package com.mm.greenstep.domain.common.config;
 
+
 import com.mm.greenstep.domain.common.jwt.JwtAuthenticationFilter;
 import com.mm.greenstep.domain.common.jwt.JwtTokenProvider;
+import com.mm.greenstep.domain.common.oauth.CustomOAuth2UserService;
+import com.mm.greenstep.domain.common.oauth.handler.OAuth2FailureHandler;
+import com.mm.greenstep.domain.common.oauth.handler.OAuth2SuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 /**
- * 스프링 시큐리티의 보안 설정 정의
+ * 설정클래스로 등록
+ * SpringSecurity 활성화
  * */
-@RequiredArgsConstructor
+@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@RequiredArgsConstructor
+public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisTemplate redisTemplate;
 
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .httpBasic().disable()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final OAuth2FailureHandler oAuth2FailureHandler;
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable() // HTTP 기본 인증을 비활성화 -> 인증안되면 기본 로그인창으로 이동
+                .cors().and() // CORS 활성화
+                .csrf().disable() // CSRF 보호 기능 비활성화 -> REST-API서버용이라서 비활성화
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션관리 정책을 STATELESS(세션이 있으면 쓰지도 않고, 없으면 만들지도 않는다)
                 .and()
+
+                // 요청에 대한 인증 설정
+                // Spring 설정클래스는 지정된 antMatchers URL순차적 탐색하여 현재 요청과 가장 먼저 매칭되는 규칙 따른다.
                 .authorizeRequests()
-                .antMatchers("user/sign-up", "user/login", "user/authority", "user/reissue", "user/logout").permitAll()
-                .antMatchers("user/userTest").hasRole("USER")
-                .antMatchers("user/adminTest").hasRole("ADMIN")
+                .antMatchers("/login/**").permitAll() // 토큰 발급을 위한 경로는 모두 허용
+                .antMatchers("/", "/css/**","/images/**","/js/**","/favicon.ico","/h2-console/**").permitAll()
+                .antMatchers("/mypage/**").hasAnyRole("USER","MANAGER") //마이페이지는 권한 있어야함
+                .anyRequest().authenticated() // 그 외의 모든 요청은 인증이 필요하다.
                 .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class);
-        // JwtAuthenticationFilter를 UsernamePasswordAuthentictaionFilter 전에 적용시킨다.
+
+                // OAuth2 로그인 설정시작
+                .oauth2Login()
+                .userInfoEndpoint().userService(customOAuth2UserService) // OAuth2 로그인시 사용자 정보를 가져오는 엔드포인트와 사용자 서비스를 설정
+                .and()
+                .failureHandler(oAuth2FailureHandler) // OAuth2 로그인 실패시 처리할 핸들러를 지정해준다.
+                .successHandler(oAuth2SuccessHandler); // OAuth2 로그인 성공시 처리할 핸들러를 지정해준다.
+
+        return http.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     // 암호화에 필요한 PasswordEncoder Bean 등록
@@ -45,4 +72,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
 }
