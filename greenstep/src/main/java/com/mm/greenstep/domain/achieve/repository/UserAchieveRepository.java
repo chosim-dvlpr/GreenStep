c18ebfa23ef95194468f9e1ce75b1ac57810ba81
ldg <eorud2546@nate.com>
2023-11-06 10:24:21 +0900

    feat : mypage dto에 완료업적 수 추가

--- greenstep/src/main/java/com/mm/greenstep/domain/achieve/repository/UserAchieveRepository.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/achieve/repository/UserAchieveRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface UserAchieveRepository extends JpaRepository<UserAchieve, Long> {
     List<UserAchieve> findAllByUserAndIsBreakedFalse(User user);
     List<UserAchieve> findAllByUserAndAchieve_AchieveType(User user, Byte achieveType);
+
+    List<UserAchieve> findAllByUserAndIsBreakedTrue(User user);
 }
