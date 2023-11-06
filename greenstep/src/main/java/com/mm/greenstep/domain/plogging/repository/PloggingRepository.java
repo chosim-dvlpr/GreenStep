6e7a1bd848c40a52d65275773f26e31494665942
ldg <eorud2546@nate.com>
2023-11-06 10:36:57 +0900

    fix : mainpage 피드 수정

--- greenstep/src/main/java/com/mm/greenstep/domain/plogging/repository/PloggingRepository.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/plogging/repository/PloggingRepository.java
@@ -16,12 +16,10 @@ public interface PloggingRepository extends JpaRepository<Plogging, Long> {
     List<Plogging> findAllByUser(User user);
 
     // 데이터베이스에서 랜덤으로 pageable개의 Plogging 레코드를 가져온 후 랜덤한 순서로 정렬하고 LIMIT를 사용하여 결과를 제한
-    @Query(value = "SELECT * FROM plogging_table WHERE is_visibled = true ORDER BY RAND() LIMIT ?1", nativeQuery = true)
-    List<Plogging> findRandomVisiblePloggingRecords(Pageable pageable);
+    @Query(value = "SELECT * FROM plogging_table WHERE is_visibled = true ORDER BY RAND() LIMIT :count", nativeQuery = true)
+    List<Plogging> findRandomVisiblePloggingRecords(@Param("count") int count);
+
 
     @Query("SELECT p FROM Plogging p WHERE FUNCTION('YEAR', p.createdAt) = :year AND p.user = :user")
     List<Plogging> findByUserAndYear(@Param("user") User user, @Param("year") int year);
-
-
-
 }
