2fbfe1d1a876c25f6f021f9ec0d69d95c37cb7d0
ldg <eorud2546@nate.com>
2023-11-06 10:45:56 +0900

    fix : mainpage 피드 수정

--- greenstep/src/main/java/com/mm/greenstep/domain/plogging/repository/PloggingRepository.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/plogging/repository/PloggingRepository.java
@@ -2,7 +2,6 @@ package com.mm.greenstep.domain.plogging.repository;
 
 import com.mm.greenstep.domain.plogging.entity.Plogging;
 import com.mm.greenstep.domain.user.entity.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +15,7 @@ public interface PloggingRepository extends JpaRepository<Plogging, Long> {
     List<Plogging> findAllByUser(User user);
 
     // 데이터베이스에서 랜덤으로 pageable개의 Plogging 레코드를 가져온 후 랜덤한 순서로 정렬하고 LIMIT를 사용하여 결과를 제한
-    @Query(value = "SELECT * FROM plogging_table WHERE is_visibled = true ORDER BY RAND() LIMIT :count", nativeQuery = true)
+    @Query(value = "SELECT * FROM Plogging WHERE is_visibled = true ORDER BY RAND() LIMIT :count", nativeQuery = true)
     List<Plogging> findRandomVisiblePloggingRecords(@Param("count") int count);
 
 
