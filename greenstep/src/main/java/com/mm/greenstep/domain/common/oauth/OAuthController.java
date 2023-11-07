c28f99d612e0e5d36231e47b064614c6efe9cbdb
LeeSW-k <swl9455@naver.com>
2023-11-07 17:47:35 +0900

    test : victory에서 date테스트

--- greenstep/src/main/java/com/mm/greenstep/domain/common/oauth/OAuthController.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/common/oauth/OAuthController.java
@@ -1,5 +1,7 @@
 package com.mm.greenstep.domain.common.oauth;
 
+import com.mm.greenstep.domain.compete.entity.Victory;
+import com.mm.greenstep.domain.compete.repository.VictoryRepository;
 import com.mm.greenstep.domain.user.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,12 +16,24 @@ import org.springframework.web.bind.annotation.*;
 public class OAuthController {
 
     private final OAuthService oAuthService;
+    private final VictoryRepository victoryRepository;
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody String accessToken) {
         String kakaoId = oAuthService.findKakaoId(accessToken);
 
         return oAuthService.findUserByKakaoId(kakaoId);
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        Victory victory = victoryRepository.findById(1).orElseThrow();
+        System.out.println(victory.getVictoryMonth().getYear());
+        System.out.println(victory.getVictoryMonth().getMonth());
+        System.out.println(victory.getVictoryMonth().getDayOfYear());
+        System.out.println(victory.getVictoryMonth().getDayOfMonth());
+
+        return null;
     }
 
 }
