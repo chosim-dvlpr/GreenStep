7c1be36ce9b9760ee5469f3bb9f9779bc72c0455
LeeSW-k <swl9455@naver.com>
2023-11-07 15:04:53 +0900

    feat : 경쟁관련 repository생성

--- greenstep/src/main/java/com/mm/greenstep/domain/compete/repository/CompeteRepository.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/compete/repository/CompeteRepository.java
@@ -1,8 +1,13 @@
 package com.mm.greenstep.domain.compete.repository;
 
 import com.mm.greenstep.domain.compete.entity.Compete;
+import com.mm.greenstep.domain.compete.entity.Victory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CompeteRepository extends JpaRepository<Compete, Long> {
+
+    List<Compete> findAllByVictory(Victory currentVictory);
 
 }
---
+++ greenstep/src/main/java/com/mm/greenstep/domain/compete/repository/VictoryRepository.java
@@ -0,0 +1,13 @@
+package com.mm.greenstep.domain.compete.repository;
+
+import com.mm.greenstep.domain.compete.entity.Victory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.YearMonth;
+import java.util.Optional;
+
+public interface VictoryRepository extends JpaRepository<Victory, Integer> {
+
+    Optional<Victory> findByVictoryMonth(YearMonth victoryMonth);
+
+}
