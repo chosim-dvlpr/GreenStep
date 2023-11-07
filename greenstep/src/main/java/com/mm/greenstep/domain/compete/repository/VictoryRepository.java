5802958c6012764f53e3c0a0b6a33478a3c5eecb
LeeSW-k <swl9455@naver.com>
2023-11-07 11:17:19 +0900

    feat : Compete - Entity, Repository생성

--- greenstep/src/main/java/com/mm/greenstep/domain/compete/repository/VictoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mm.greenstep.domain.compete.repository;
-
-import com.mm.greenstep.domain.compete.entity.Victory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VictoryRepository extends JpaRepository<Victory, Long> {
-
-}
