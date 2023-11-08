c2bf33c5bbdc803f50e8b3029e9a41c2e6c2dec1
Minji Byun <jwww98730@naver.com>
2023-11-08 16:14:29 +0900

    fix: 플로깅 업적 모달 초기화

--- FrontEnd/GreenStep/src/Component/PloggingFinish/PlogginFinishAchievementModal.tsx
+++ FrontEnd/GreenStep/src/Component/PloggingFinish/PlogginFinishAchievementModal.tsx
@@ -1,12 +1,1 @@
-import { View, Text } from 'react-native'
-import React from 'react'
 
-const PlogginFinishAchievementModal = () => {
-  return (
-    <View>
-      <Text>PlogginFinishAchievementModal</Text>
-    </View>
-  )
-}
-
-export default PlogginFinishAchievementModal
