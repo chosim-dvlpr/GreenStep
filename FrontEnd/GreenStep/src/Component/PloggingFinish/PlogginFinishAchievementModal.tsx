8f7bdebc7799ff1473d390e1245a528dfe72a904
Minji Byun <jwww98730@naver.com>
2023-11-08 10:22:44 +0900

    feat: 모달 세팅

---
+++ FrontEnd/GreenStep/src/Component/PloggingFinish/PlogginFinishAchievementModal.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PlogginFinishAchievementModal = () => {
+  return (
+    <View>
+      <Text>PlogginFinishAchievementModal</Text>
+    </View>
+  )
+}
+
+export default PlogginFinishAchievementModal
