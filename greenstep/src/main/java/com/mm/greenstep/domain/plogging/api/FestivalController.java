c50f24c508ff565fc60041dbe4dc81b1da4fefd5
ldg <eorud2546@nate.com>
2023-11-13 21:00:07 +0900

    feat : 행사정보 크롤링, 가져오기 구현

--- greenstep/src/main/java/com/mm/greenstep/domain/plogging/api/FestivalController.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/plogging/api/FestivalController.java
@@ -1,14 +1,11 @@
 package com.mm.greenstep.domain.plogging.api;
 
-import com.mm.greenstep.domain.plogging.dto.request.PloggingReqDto;
-import com.mm.greenstep.domain.plogging.dto.response.PloggingResDto;
 import com.mm.greenstep.domain.plogging.dto.response.getFestivalResDto;
 import com.mm.greenstep.domain.plogging.service.FestivalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -17,7 +14,6 @@ import java.util.List;
 public class FestivalController {
     private final FestivalService festivalService;
 
-    // 플로깅 종료
     @GetMapping
     public ResponseEntity<?> getFestivalInfo() {
         List<getFestivalResDto> festivalList = festivalService.getFestivalInfo();
