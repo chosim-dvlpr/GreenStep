418a0a32d0b7173e44da7eaa702b2e32e11460cd
ldg <eorud2546@nate.com>
2023-11-14 10:37:46 +0900

    feat : 업적 갱신 구현

--- greenstep/src/main/java/com/mm/greenstep/domain/plogging/entity/Trash.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/plogging/entity/Trash.java
@@ -24,20 +24,11 @@ public class Trash {
     @JoinColumn(name = "plogging_id")
     private Plogging plogging;
 
-//    @Column(name = "trash_count")
-//    private Long trashCount;
-
     @Column(name = "latitude", nullable = false)
     private String latitude;
 
     @Column(name = "longitude", nullable = false)
     private String longitude;
-
-//    @Column(name = "is_picture", nullable = false)
-//    private Boolean isPicture;
-//
-//    @Column(name = "trash_picture")
-//    private String trashPicture;
 
     @Column(name = "trash_type")
     private Byte trashType;
