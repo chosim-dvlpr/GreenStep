f7916623c65f6c45fd06e97547c1c81222d2541d
이주언 <wndjs8225@naver.com>
2023-11-16 01:00:23 +0900

    feat : 플로깅 참여 추가

--- FrontEnd/GreenStep/src/Component/Board/BoardCRUDInfo.tsx
+++ FrontEnd/GreenStep/src/Component/Board/BoardCRUDInfo.tsx
@@ -1,55 +1,72 @@
-import { View, Text, TouchableOpacity, ScrollView, Image } from "react-native"
+import { View, Text, TouchableOpacity, ScrollView, Image, SafeAreaView, TextInput, StyleSheet } from "react-native"
 import Box from "../../Style/Box";
 import { Calendar, DateData } from "react-native-calendars";
-import { useState } from "react";
 import plus from '../../Image/Board/plus.png'
 import minus from '../../Image/Board/minus.png'
-const BoardCRUDInfo = () => {
-    const [day, setDay] = useState('날짜 선택')
-    const [showCalendar, setShowCalendar] = useState(false)
-    const [join, setJoin] = useState(0)
 
-    const handleDay = (day:DateData) => {
-        setDay(day.dateString)
-        setShowCalendar(false)
-    }
-    const handleShow = () =>{
-        setShowCalendar(!showCalendar)
-    }
-    const handleJoinPlus = () => {
-        setJoin(join+1)
-    }
-    const handleJoinMinus = () => {
-        if (join > 0) {
-            setJoin(join-1)
-        }
-    }
+interface CreateInfoProps {
+    day? : string;
+    location? : string;
+    join? : number;
+    showCalendar: boolean;
+    handleJoinMinus: () => void;
+    handleJoinPlus: () => void;
+    onLocateChange: (locate: string) => void;
+    handleDay: (date: DateData) => void;
+    handleShow: () => void;
+}
+
+const BoardCRUDInfo = (props: CreateInfoProps) => {
+    
     return(
         <ScrollView style={{marginBottom: 20}}>
-            <View style={{justifyContent:'center', alignItems:'center'}}>
-        <View style={[Box.cardBox, {display:'flex'}]}>
-            <Text style={{marginBottom: 10, fontSize:20}}>Information</Text>
-            <View style={[Box.flexRowBox,{marginBottom: 10, justifyContent:'space-between' }]}>
-                <Text style={{fontSize: 16}}>모집인원</Text>
-                <TouchableOpacity onPress={handleJoinMinus}>
-                    <Image source={minus}></Image>
-                </TouchableOpacity>
-                <Text style={{fontSize: 16}}>{join}</Text>
-                <TouchableOpacity onPress={handleJoinPlus}>
-                    <Image source={plus}></Image>
-                </TouchableOpacity>
-            </View>
+        <View style={{justifyContent:'center', alignItems:'center'}}>
+            <View style={[Box.cardBox, {display:'flex'}]}>
+                <Text style={{marginBottom: 10, fontSize:20}}>Information</Text>
 
-            <View style={[Box.flexRowBox,{marginBottom: 10, justifyContent:'space-between' }]}>
-                    <Text style={{fontSize: 16}}>활동날짜</Text>
-                <TouchableOpacity onPress={handleShow}>
-                    <Text style={{fontSize: 16, color: "#72BF6A"}}>{day}</Text>
-                </TouchableOpacity>
+                <View style={[Box.flexRowBox,{marginBottom: 10, justifyContent:'space-between' }]}>
+                    <Text style={{fontSize: 20}}>지역</Text>
+                    <SafeAreaView>
+                        <TextInput style={styles.input} placeholder="지역을 입력하세요"
+                                onChangeText={props.onLocateChange} value={props.location} />
+                    </SafeAreaView>
+                </View>
+                
+                <View style={[Box.flexRowBox,{marginBottom: 10, justifyContent:'space-between' }]}>
+                    <Text style={{fontSize: 20}}>모집인원</Text>
+                    <TouchableOpacity onPress={props.handleJoinMinus}>
+                        <Image source={minus}></Image>
+                    </TouchableOpacity>
+                    <Text style={{fontSize: 25}}>{props.join}</Text>
+                    <TouchableOpacity onPress={props.handleJoinPlus}>
+                        <Image source={plus}></Image>
+                    </TouchableOpacity>
+                </View>
+
+                <View style={[Box.flexRowBox,{marginBottom: 10, justifyContent:'space-between' }]}>
+                    <Text style={{fontSize: 20}}>활동날짜</Text>
+                    <TouchableOpacity onPress={props.handleShow}>
+                        <Text style={{fontSize: 20}}>{props.day}</Text>
+                    </TouchableOpacity>
+                </View>
+
+            {props.showCalendar && <Calendar onDayPress={props.handleDay}></Calendar>}
             </View>
-            {showCalendar && <Calendar onDayPress={handleDay}></Calendar>}
         </View>
-        </View>
-        </ScrollView>
+    </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    input: {
+      width: 150,
+      height: 35,
+      marginHorizontal: 20,
+      marginBottom: 5,
+      borderWidth: 1,
+      padding: 10,
+      borderRadius: 10,
+    },
+  });
+
 export default BoardCRUDInfo;
