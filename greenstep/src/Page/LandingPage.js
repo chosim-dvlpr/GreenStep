90ef8c91796ba733e1899f54301e240e8adf50db
LeeSW-k <swl9455@naver.com>
2023-11-16 14:23:55 +0900

    feat : main페이지

--- greenstep/src/Page/LandingPage.js
+++ greenstep/src/Page/LandingPage.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './LandingPage.css'; // 스타일시트 임포트
 
 export default function LandingPage() {
   return (
-    <div>LandingPage</div>
-  )
+    <div className="landing-container">
+      <video autoPlay loop muted className="background-video">
+        <source src="/plogging1.mp4" type="video/mp4"/>
+        Your browser does not support the video tag.
+      </video>
+      <div className="overlay-text">GreenStep</div>
+    </div>
+  );
 }
