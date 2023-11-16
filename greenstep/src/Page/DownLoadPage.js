56eacfe7c7fd3d7eb5c301c454ae5bc71a7b4db5
LeeSW-k <swl9455@naver.com>
2023-11-16 22:02:54 +0900

    style : icon 변경

--- greenstep/src/Page/DownLoadPage.js
+++ greenstep/src/Page/DownLoadPage.js
@@ -5,8 +5,8 @@ export default function DownLoadPage() {
   return (
     <div className='download-page'>
       <div className="title-text">Green Step</div>
-      <button className="rounded-button1">버튼</button> 
-      <button className="rounded-button2">버튼</button> 
+      <button className="rounded-button1">GreenStep 하러가기</button> 
+      <button className="rounded-button2">CSV Download</button> 
     </div>
   );
 }
