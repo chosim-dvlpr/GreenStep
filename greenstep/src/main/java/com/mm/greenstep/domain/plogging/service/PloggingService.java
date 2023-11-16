9d9c6706ccaca34fd2d9ab2816fe112e4e4d4962
ldg <eorud2546@nate.com>
2023-11-16 22:33:42 +0900

    fix : TraveRange km단위로 수정

--- greenstep/src/main/java/com/mm/greenstep/domain/plogging/service/PloggingService.java
+++ greenstep/src/main/java/com/mm/greenstep/domain/plogging/service/PloggingService.java
@@ -69,8 +69,8 @@ public class PloggingService {
         User user = userRepository.findByUserId(user_pk);
 
         // 대한민국 시간대로 현재 시간을 설정
-//        LocalDateTime endTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
-        LocalDateTime endTime = dto.getNow();
+        LocalDateTime endTime = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+//        LocalDateTime endTime = dto.getNow();
 
 
         // dto.getTravelTime()이 초 단위로 시간을 반환한다고 가정
