7e59d2ef3689cbc43de281d10a9bc8cb70c4eba2
LeeSW-k <swl9455@naver.com>
2023-11-16 21:25:57 +0900

    style : main페이지 디자인 최종

--- greenstep/src/Page/LandingPage.js
+++ greenstep/src/Page/LandingPage.js
@@ -23,12 +23,35 @@ export default function LandingPage() {
       <video autoPlay loop muted className="background-video">
         <source src="/plogging1.mp4" type="video/mp4"/>
       </video>
-      <div className="overlay-text1 overlay-text">time</div>
-      <div className="overlay-text1 overlay-text-sub">{data.travelTime || 'Loading...'}</div>
-      <div className="overlay-text2 overlay-text">range</div>
-      <div className="overlay-text2 overlay-text-sub">{data.travelRange || 'Loading...'}</div>
-      <div className="overlay-text3 overlay-text">trash</div>
-      <div className="overlay-text3 overlay-text-sub">{data.trashAmount || 'Loading...'}</div>
+      <div className="overlay-text1 overlay-text-top" style={{ top: "63.5%" }}><img className='imagesize' src="/icon/time.gif" alt="time"/></div>
+      <div className="overlay-text1 overlay-text">{data.travelTime ? (data.travelTime.toString().length > 3 ? 
+                                                    data.travelTime.toLocaleString() : data.travelTime) : 'Loading...'}</div>
+      <div className="overlay-text1 overlay-text-sub">
+        <div className='inlineBlock point'>Hour</div>
+        <div className='inlineBlock'>&nbsp;of </div>
+        <div className='inlineBlock'> &nbsp;Green</div>
+        <div className='inlineBlock'>Step</div>
+      </div>
+
+      <div className="overlay-text2 overlay-text-top"><img className='imagesize' src="/icon/walking.png" alt="walking"/></div>
+      <div className="overlay-text2 overlay-text">{data.travelRange ? (data.travelRange.toFixed(0).length > 3 ? Number(data.travelRange.toFixed(0)).toLocaleString() 
+                                                    : data.travelRange.toFixed(0)) : 'Loading...'}</div>
+      <div className="overlay-text2 overlay-text-sub">
+        <div className='inlineBlock point'>KM</div>
+        <div className='inlineBlock'>&nbsp;of </div>
+        <div className='inlineBlock'> &nbsp;Green</div>
+        <div className='inlineBlock'>Step</div>
+      </div>
+
+      <div className="overlay-text3 overlay-text-top"><img className='imagesize2' src="/icon/trash.png" alt="trash"/></div>
+      <div className="overlay-text3 overlay-text">{data.trashAmount ? (data.trashAmount.toString().length > 3 ? 
+                                                    data.trashAmount.toLocaleString() : data.trashAmount) : 'Loading...'}</div>
+      <div className="overlay-text3 overlay-text-sub">
+        <div className='inlineBlock point'>Trash</div>
+        <div className='inlineBlock'>&nbsp;of </div>
+        <div className='inlineBlock'> &nbsp;Green</div>
+        <div className='inlineBlock'>Step</div>
+      </div>
     </div>
   );
 }
